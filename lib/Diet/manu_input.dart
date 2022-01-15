ba324a467f54fdaa97afb6e664c78e2d8b4e3661
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2022-01-15 13:53:21 -0500

    Update manu_input.dart

--- lib/Diet/manu_input.dart
+++ lib/Diet/manu_input.dart
@@ -8,33 +8,92 @@ class manu_input extends StatefulWidget {
 }
 
 class _manu_inputState extends State<manu_input> {
-  String drop = "chicken";
+  String drop1 = "chicken";
+  String drop2 = "Orange Juice";
+  String drop3 = "Pudding";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Manual Input your meal")),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            DropdownButton(
-                value: drop,
-                onChanged: (value) {
-                  setState(() {
-                    drop = value.toString();
-                  });
-                },
-                items: <String>[
-                  "chicken",
-                  "beef",
-                  "pork",
-                  "noodle",
-                  "rice",
-                ].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList()),
+            Row(
+              children: [
+                const Text("Main Clause: "),
+                DropdownButton(
+                    value: drop1,
+                    onChanged: (value) {
+                      setState(() {
+                        drop1 = value.toString();
+                      });
+                    },
+                    items: <String>[
+                      "chicken",
+                      "beef",
+                      "pork",
+                      "noodle",
+                      "rice",
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList())
+              ],
+            ),
+            Row(
+              children: [
+                const Text("Drink: "),
+                DropdownButton(
+                    value: drop2,
+                    onChanged: (value) {
+                      setState(() {
+                        drop2 = value.toString();
+                      });
+                    },
+                    items: <String>[
+                      "Orange Juice",
+                      "Water",
+                      "Wine",
+                      "Sparkiling Water",
+                      "None",
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList())
+              ],
+            ),
+            Row(
+              children: [
+                const Text("Desert: "),
+                DropdownButton(
+                    value: drop3,
+                    onChanged: (value) {
+                      setState(() {
+                        drop3 = value.toString();
+                      });
+                    },
+                    items: <String>[
+                      "Pudding",
+                      "Cake",
+                      "None",
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList())
+              ],
+            ),
+            RaisedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                label: Text("Add to my record"))
           ],
         ),
       ),
